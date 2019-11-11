b0d231b60bbb2939a7476c6bb4da411b303cc9e1
ferhatucar <ucarferhat3@gmail.com>
2019-11-11 15:36:50 +0100

    adds payload for user with json web token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 // Get the user model
@@ -46,6 +48,16 @@ router.post('/', [
         // saves the user on the database
         await user.save();
         res.send('User saved');
+
+        const payload = { user: { id: user.id }};
+
+        // creates json web token
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token })
+        });
 
     } catch (err) {
         // logs the error on the console
