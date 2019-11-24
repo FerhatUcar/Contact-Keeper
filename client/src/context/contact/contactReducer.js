14e2ceae1945fdca3cf0d88fa510f2f59d6cf4bd
ferhatucar <ucarferhat3@gmail.com>
2019-11-24 21:39:50 +0100

    updates reducer with new cases

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -22,23 +22,34 @@ export default (state, action) => {
             };
         case ADD_CONTACT:
             return {
+                // get the current state
                 ...state,
+
+                // the state is immutable, so copy the state with contacts
                 contacts: [action.payload, ...state.contacts],
+
                 loading: false
             };
         case UPDATE_CONTACT:
             return {
                 ...state,
+
+                // map through all of the contacts
+                // its going to look for the id the one that the payload has
+                // if its matches its going to send the new updated contact and replace that with the current one
+                // else its going to return whatever the original contact is
                 contacts: state.contacts.map(contact =>
-                    contact._id === action.payload._id ? action.payload : contact
+                    contact.id === action.payload.id ? action.payload : contact
                 ),
                 loading: false
             };
         case DELETE_CONTACT:
             return {
                 ...state,
+
+                // filter the contact from the UI
                 contacts: state.contacts.filter(
-                    contact => contact._id !== action.payload
+                    contact => contact.id !== action.payload
                 ),
                 loading: false
             };
