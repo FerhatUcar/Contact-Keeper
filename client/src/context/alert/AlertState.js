27c14652dd8ae8fcb9f957efaa558a8c1a9515df
ferhatucar <ucarferhat3@gmail.com>
2019-12-05 19:57:29 +0100

    updates alert state and reducer in context

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,26 +1,35 @@
 import React, {useReducer} from 'react';
+import uuid from 'uuid';
+
 import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
+
 import {SET_ALERT, REMOVE_ALERT} from '../types';
 
 const AlertState = props => {
-    const initialState = null;
+    const initialState = [];
     const [state, dispatch] = useReducer(AlertReducer, initialState);
 
     // Set alert
-    const setAlert = (msg, type) => {
+    const setAlert = (msg, type, timeout = 5000) => {
+        // generate random id
+        const id = uuid.v4();
+
         // show alert when no value is passed
         dispatch({
             type: SET_ALERT,
-            payload: {msg, type}
+            payload: { msg, type, id }
         });
 
         // remove alert after 5 seconds
-        setTimeout(() => dispatch({type: REMOVE_ALERT}), 5000);
+        setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
     };
 
     return (
-        <AlertContext.Provider value={{alert: state, setAlert}}>
+        <AlertContext.Provider value={{
+            alerts: state,
+            setAlert
+        }}>
             {props.children}
         </AlertContext.Provider>
     )};
