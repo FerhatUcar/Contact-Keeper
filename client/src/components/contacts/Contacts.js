8504d1f86e1c333204ad9b5724ec8aba10d852f9
ferhatucar <ucarferhat3@gmail.com>
2019-12-11 13:51:08 +0100

    updates contact and contact item component with fixed id's, new states and adds spinner

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -4,31 +4,30 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import Spinner from '../layout/Spinner';
 
-// context
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     // initialize contact and access to all states
     const contactContext = useContext(ContactContext);
-    const { contacts, filtered } = contactContext;
+    const { contacts, filtered, getContacts, loading } = contactContext;
 
     useEffect(() => {
-        // getContacts();
+        getContacts();
         // eslint-disable-next-line
     }, []);
 
-    if (contacts !== null && contacts.length === 0) {
+    if (contacts !== null && contacts.length === 0 && !loading) {
         return <h4>Please add a contact</h4>;
     }
 
     return (
         <Fragment>
-            {contacts !== null ? (
+            {contacts !== null && !loading ? (
                 <TransitionGroup>
                     {filtered !== null
                         ? filtered.map(contact => (
                             <CSSTransition
-                                key={contact.id}
+                                key={contact._id}
                                 timeout={500}
                                 classNames='item'
                             >
@@ -37,7 +36,7 @@ const Contacts = () => {
                         ))
                         : contacts.map(contact => (
                             <CSSTransition
-                                key={contact.id}
+                                key={contact._id}
                                 timeout={500}
                                 classNames='item'
                             >
