647b68caceca8a25e9d3e17bed07510537c2500b
ferhatucar <ucarferhat3@gmail.com>
2019-12-12 12:57:55 +0100

    adds path module in server file, adds production json file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -22,6 +23,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) =>
+        // look into the current folder, then client, then build and
+        // then load index file
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     );
 }
