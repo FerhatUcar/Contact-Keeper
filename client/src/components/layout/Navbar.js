9e64bddaf3d037f99a8b101fac5926cc840c86ac
ferhatucar <ucarferhat3@gmail.com>
2019-12-14 18:48:03 +0100

    updates navbar code

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,30 +1,30 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from "../../context/auth/authContext";
 import ContactContext from "../../context/contact/contactContext";
-
 
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
 
-const Navbar = ({ title, title2, icon }) => {
+const Navbar = ({ title, title2 }) => {
     const authContext = useContext(AuthContext);
     const contactContext = useContext(ContactContext);
 
     const { logout, user, isAuthenticated } = authContext;
-    // const { clearContacts } = contactContext;
+    const { clearContacts } = contactContext;
 
-    const [ anchorEl, setAnchorEl ] = React.useState(null);
+    const [ anchorEl, setAnchorEl ] = useState(null);
 
-    const handleClick = event => setAnchorEl(event.currentTarget);
+    const handleClick = e => setAnchorEl(e.currentTarget);
+
     const handleClose = () => setAnchorEl(null);
 
     const onLogout = () => {
         logout();
-        // clearContacts();
+        clearContacts();
     };
 
     // check if there is a user
@@ -34,7 +34,9 @@ const Navbar = ({ title, title2, icon }) => {
             <li>
                 <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
                     <i className="fas fa-user" />
-                    <span className="navbar__name">{ user && user.name }</span>
+                    <span className="navbar__name">
+                        { user && user.name }
+                    </span>
                     <i className="fas fa-chevron-down"/>
                 </Button>
             </li>
@@ -43,7 +45,8 @@ const Navbar = ({ title, title2, icon }) => {
                 anchorEl={anchorEl}
                 keepMounted
                 open={Boolean(anchorEl)}
-                onClose={handleClose}>
+                onClose={handleClose}
+            >
                 <MenuItem onClick={handleClose}>
                     <Link to="/" className="menu-link">
                         <i className="fas fa-home" /> Home
